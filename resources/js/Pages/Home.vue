9e008bd00cd7e5fcfe9dfbf98ce5a228962f87fb
mabio <mabiao@wudi360.com>
2023-09-27 10:13:36 +0800

    feat: main layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
-
+import MainLayout from '@/Layouts/MainLayout.vue'
 </script>
 
 <template>
     <Head title="Home" />
-    <div class="bg-green-500">Home view</div>
+    <MainLayout>
+        <div class="bg-green-500">Home view</div>
+    </MainLayout>  
 
 </template>
 
