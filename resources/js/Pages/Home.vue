114ffc3a1c127e9f714cb2d42347518c9b4c256a
mabio <mabiao@wudi360.com>
2023-09-27 12:58:59 +0800

    feat: menu-item main-layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,7 +5,6 @@ import MainLayout from '@/Layouts/MainLayout.vue'
 <template>
     <Head title="Home" />
     <MainLayout>
-        <div class="bg-green-500">Home view</div>
     </MainLayout>  
 
 </template>
